fd20eabc9bd487a9e2c88982e9d2200ce1023c88
Russell Haering <russell@edgebit.io>
2022-11-09 15:09:53 -0800

    Add enclave handle interface

---
+++ enclave.go
@@ -0,0 +1,121 @@
+package enclave
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"errors"
+	"sync"
+
+	"github.com/hf/nsm"
+	"github.com/hf/nsm/request"
+)
+
+const (
+	defaultKeyBits = 2048
+)
+
+var (
+	globalHandle        *EnclaveHandle
+	initializationError error
+	initMutex           sync.Mutex
+)
+
+type AttestationOptions struct {
+	// Nonce is an optional cryptographic nonce which may be signed as part of the attestation
+	// for use by applications in preventing replay attacks.
+	Nonce []byte
+
+	// UserData is an optional opaque blob which will be signed as part of the attestation
+	// for application-defined purposes.
+	UserData []byte
+
+	// NoPublicKey will prevent the defaul public key from being included in the attestation.
+	NoPublicKey bool
+
+	// PublicKey is an optional public key which will be included in the attestation. Valid types
+	// are *rsa.PublicKey, *ecdsa.PublicKey, and ed25519.PublicKey.
+	PublicKey any
+}
+
+type EnclaveHandle struct {
+	nsm *nsm.Session
+	key *rsa.PrivateKey
+}
+
+func (enclave *EnclaveHandle) initialize() error {
+	var err error
+
+	enclave.nsm, err = nsm.OpenDefaultSession()
+	if err != nil {
+		return err
+	}
+
+	enclave.key, err = rsa.GenerateKey(rand.Reader, defaultKeyBits)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Obtain an attestation document from the enclave's Nitro Security Module.
+// See AttestationOptions for
+func (enclave *EnclaveHandle) Attest(args AttestationOptions) ([]byte, error) {
+	var publicKey []byte
+	var err error
+	if args.PublicKey != nil && !args.NoPublicKey {
+		publicKey, err = x509.MarshalPKIXPublicKey(args.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+	} else if !args.NoPublicKey {
+		publicKey, err = x509.MarshalPKIXPublicKey(enclave.PublicKey())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	res, err := enclave.nsm.Send(&request.Attestation{
+		Nonce:     args.Nonce,
+		UserData:  args.UserData,
+		PublicKey: publicKey,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Error != "" {
+		return nil, errors.New(string(res.Error))
+	}
+
+	if res.Attestation == nil || res.Attestation.Document == nil {
+		return nil, errors.New("attestation response missing attestation document")
+	}
+
+	return res.Attestation.Document, nil
+}
+
+func (enclave *EnclaveHandle) PublicKey() *rsa.PublicKey {
+	return &enclave.key.PublicKey
+}
+
+func (enclave *EnclaveHandle) PrivateKey() *rsa.PrivateKey {
+	return enclave.key
+}
+
+func GetOrInitializeHandle() (*EnclaveHandle, error) {
+	initMutex.Lock()
+	defer initMutex.Unlock()
+
+	if globalHandle == nil && initializationError == nil {
+		enclave := &EnclaveHandle{}
+		if err := enclave.initialize(); err != nil {
+			initializationError = err
+		} else {
+			globalHandle = enclave
+		}
+	}
+
+	return globalHandle, initializationError
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/edgebitio/nitro-enclaves-sdk-go
+
+go 1.19
+
+require (
+	github.com/hf/nsm v0.0.0-20220930140112-cd181bd646b9
+	github.com/stretchr/testify v1.8.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
